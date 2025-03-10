259c938121883a2abd3b4143911dc56b1bfb7aab
cflb <iamcflb@gmail.com>
2025-03-09 23:49:24 -0300

    add dev env for databases - use postgres or sqlite

--- app-proj/packhousecontroll/packhousecontroll/settings.py
+++ app-proj/packhousecontroll/packhousecontroll/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w48+wj4bf(1%5xfw8tl^i$g9pctm^#&6_q5*dbxig79%8=6j(e'
-
+SECRET_KEY = os.getenv('SECRET_KEY') == 'True'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
@@ -80,27 +79,27 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'packhousecontroll.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-# 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB', 'mydatabase'),
-        'USER': os.environ.get('POSTGRES_USER', 'myuser'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'mypassword'),
-        'HOST': os.environ.get('POSTGRES_HOST', 'db'), # Default to 'db' for services
-        'PORT': os.environ.get('POSTGRES_PORT', 5432),
+USE_POSTGRES = os.environ.get('USE_POSTGRES', 'False').lower() in ['true', '1']
+
+if USE_POSTGRES:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('POSTGRES_DB', 'mydatabase'),
+            'USER': os.environ.get('POSTGRES_USER', 'myuser'),
+            'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'mypassword'),
+            'HOST': os.environ.get('POSTGRES_HOST', 'db'),
+            'PORT': os.environ.get('POSTGRES_PORT', 5432),
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
